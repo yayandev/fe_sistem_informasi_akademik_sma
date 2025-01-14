321eb87ac10ecf7daa16c3c26fe43deda98a18b0
yayandev <yayanfathurohman20@gmail.com>
2025-01-14 15:00:52 +0700

    fix: fix scroll menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -29,7 +29,7 @@ export default function DashboardLayout({
       />
       <div className="h-screen flex">
         {/* LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <div className="w-[14%] md:w-[8%] lg:w-[18%] p-4">
           <Link
             href="/"
             className="flex items-center justify-center lg:justify-start gap-2"
